4381072739e3dec488ff9a8b2e2aaa4a22be613c
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-01-20 11:15:04 +0530

    Firebase and Git init

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions'),
+	express = require('express'),
+	app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+	res.render('index');
+});
+
+app.use((req, res, next) => {
+	res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);

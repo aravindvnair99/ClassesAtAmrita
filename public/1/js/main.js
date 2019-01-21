468ece35c046a0e3eb90a4d2101ab0bc56fbf697
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-01-21 16:35:11 +0530

    Refactor

--- public/1/js/main.js
+++ public/1/js/main.js
@@ -1,168 +1,166 @@
 /*global $, jQuery, alert*/
 $(document).ready(function() {
+	'use strict';
 
-  'use strict';
+	// ========================================================================= //
+	//  //SMOOTH SCROLL
+	// ========================================================================= //
 
-  // ========================================================================= //
-  //  //SMOOTH SCROLL
-  // ========================================================================= //
+	$(document).on('scroll', onScroll);
 
+	$('a[href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		$(document).off('scroll');
 
-  $(document).on("scroll", onScroll);
+		$('a').each(function() {
+			$(this).removeClass('active');
+			if ($(window).width() < 768) {
+				$('.nav-menu').slideUp();
+			}
+		});
 
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+		$(this).addClass('active');
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
+		var target = this.hash,
+			menu = target;
 
-    $(this).addClass('active');
-
-    var target = this.hash,
-        menu = target;
-
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
-
-
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
-
-  // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
-
-
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
-
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
-
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
-  });
-
-  // ========================================================================= //
-  //  Typed Js
-  // ========================================================================= //
-
-  var typed = $(".typed");
-
-  $(function() {
-    typed.typed({
-		strings: [
-			'Hello!',
-			'നമസ്കാരം!',
-			'வணக்கம்!',
-			'నమస్కారం!',
-			'ನಮಸ್ಕಾರ!',
-			'स्वागत!',
-			'नमस्ते!',
-			'¡Hola!',
-			'Bonjour!',
-			'Hallo!'
-		],
-		typeSpeed: 100,
-		loop: true
+		target = $(target);
+		$('html, body')
+			.stop()
+			.animate(
+				{
+					scrollTop: target.offset().top - 80
+				},
+				500,
+				'swing',
+				function() {
+					window.location.hash = target.selector;
+					$(document).on('scroll', onScroll);
+				}
+			);
 	});
-  });
 
+	function onScroll(event) {
+		if ($('.home').length) {
+			var scrollPos = $(document).scrollTop();
+			$('nav ul li a').each(function() {
+				var currLink = $(this);
+				var refElement = $(currLink.attr('href'));
+			});
+		}
+	}
 
-  // ========================================================================= //
-  //  Owl Carousel Services
-  // ========================================================================= //
+	// ========================================================================= //
+	//  //NAVBAR SHOW - HIDE
+	// ========================================================================= //
 
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if (scroll > 200) {
+			$('#main-nav, #main-nav-subpage').slideDown(700);
+			$('#main-nav-subpage').removeClass('subpage-nav');
+		} else {
+			$('#main-nav').slideUp(700);
+			$('#main-nav-subpage').hide();
+			$('#main-nav-subpage').addClass('subpage-nav');
+		}
+	});
 
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
+	// ========================================================================= //
+	//  // RESPONSIVE MENU
+	// ========================================================================= //
 
+	$('.responsive').on('click', function(e) {
+		$('.nav-menu').slideToggle();
+	});
 
-  // ========================================================================= //
-  //  Porfolio isotope and filter
-  // ========================================================================= //
+	// ========================================================================= //
+	//  Typed Js
+	// ========================================================================= //
 
+	var typed = $('.typed');
 
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
+	$(function() {
+		typed.typed({
+			strings: [
+				'Hello!',
+				'നമസ്കാരം!',
+				'வணக்கம்!',
+				'నమస్కారం!',
+				'ನಮಸ್ಕಾರ!',
+				'स्वागत!',
+				'नमस्ते!',
+				'¡Hola!',
+				'Bonjour!',
+				'Hallo!'
+			],
+			typeSpeed: 100,
+			loop: true
+		});
+	});
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+	// ========================================================================= //
+	//  Owl Carousel Services
+	// ========================================================================= //
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
+	$('.services-carousel').owlCarousel({
+		autoplay: true,
+		loop: true,
+		margin: 20,
+		dots: true,
+		nav: false,
+		responsiveClass: true,
+		responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+	});
 
+	// ========================================================================= //
+	//  Porfolio isotope and filter
+	// ========================================================================= //
 
-  // ========================================================================= //
-  //  magnificPopup
-  // ========================================================================= //
+	var portfolioIsotope = $('.portfolio-container').isotope({
+		itemSelector: '.portfolio-thumbnail',
+		layoutMode: 'fitRows'
+	});
 
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
+	$('#portfolio-flters li').on('click', function() {
+		$('#portfolio-flters li').removeClass('filter-active');
+		$(this).addClass('filter-active');
 
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+		portfolioIsotope.isotope({ filter: $(this).data('filter') });
+	});
 
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
+	// ========================================================================= //
+	//  magnificPopup
+	// ========================================================================= //
 
+	var magnifPopup = function() {
+		$('.popup-img').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery: {
+				enabled: true
+			},
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
 
-  // Call the functions
-  magnifPopup();
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
 
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+					// openerElement is the element on which popup was initialized, in this case its <a> tag
+					// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+					return openerElement.is('img')
+						? openerElement
+						: openerElement.find('img');
+				}
+			}
+		});
+	};
+
+	// Call the functions
+	magnifPopup();
 });
